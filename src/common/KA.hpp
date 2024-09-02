36582186e8a0a47c2e0a291a8231e02da50d4eb2
XXMZHH <150583044+XXMZHH@users.noreply.github.com>
2024-09-02 09:58:00 +0800

    Update KA.hpp

--- src/common/KA.hpp
+++ src/common/KA.hpp
@@ -9,44 +9,37 @@
 
 namespace KA
 {
-std::vector<int> compute_shared_secret(EVP_PKEY *pkey, EVP_PKEY *peerkey) {//这里传入的是两个不同用户的密钥
+std::vector<int> compute_shared_secret(EVP_PKEY *pkey, EVP_PKEY *peerkey) {
     EVP_PKEY_CTX *ctx;
-    //声明一个 unsigned char 类型的向量 secret_bytes，这个向量将用来存储生成的共享密钥的原始字节数据。
+   
     std::vector<unsigned char> secret_bytes;
-    //声明一个 int 类型的向量 secret，用于存储处理后的共享密钥数据。在这个特定的代码中，共享密钥将被转换为一个或多个整数，然后存储在这个向量中。
     std::vector<int> secret;
-    //声明一个 size_t 类型的变量 secret_len，用来存储密钥派生函数生成的共享密钥的长度（以字节为单位）。这个长度是动态确定的，依赖于使用的密钥协议和公钥的类型。
     size_t secret_len;
 
-    //创建一个 EVP_PKEY_CTX 对象，用于执行密钥派生操作。这个对象将包含密钥和密钥派生函数的参数。
     ctx = EVP_PKEY_CTX_new(pkey, NULL);
     if (!ctx) {
         std::cerr << "EVP_PKEY_CTX_new failed" << std::endl;
         return {};
     }
-// 初始化密钥派生操作
+
     if (EVP_PKEY_derive_init(ctx) <= 0) {
         std::cerr << "EVP_PKEY_derive_init failed" << std::endl;
         EVP_PKEY_CTX_free(ctx);
         return {};
     }
     
-    // 设置对等方公钥，将对方的公钥 peerkey 设置到派生上下文中。
-    //这一步是密钥协商中的一个重要部分，确保了两个公钥能够用于生成共享密钥。如果设置失败，同样会清理资源并返回。
+
     if (EVP_PKEY_derive_set_peer(ctx, peerkey) <= 0) {
         std::cerr << "EVP_PKEY_derive_set_peer failed" << std::endl;
         EVP_PKEY_CTX_free(ctx);
         return {};
     }
-// 获取密钥派生函数生成的共享密钥的长度
-//传入 NULL 作为第一个参数，用来获取必要的缓冲区长度 (secret_len)。这是为了准备足够的空间存储即将生成的共享密钥。
+
     if (EVP_PKEY_derive(ctx, NULL, &secret_len) <= 0) {
         std::cerr << "EVP_PKEY_derive failed to determine buffer length" << std::endl;
         EVP_PKEY_CTX_free(ctx);
         return {};
     }
-//生成共享密钥
-//调整 secret_bytes 的大小以匹配预计的长度 (secret_len)，然后再次调用 EVP_PKEY_derive，这次是为了实际生成密钥，并将其存储到 secret_bytes 中。
     secret_bytes.resize(secret_len);
     if (EVP_PKEY_derive(ctx, secret_bytes.data(), &secret_len) <= 0) {
         std::cerr << "EVP_PKEY_derive failed" << std::endl;
@@ -55,7 +48,6 @@ std::vector<int> compute_shared_secret(EVP_PKEY *pkey, EVP_PKEY *peerkey) {//这
 
     EVP_PKEY_CTX_free(ctx);
     
-    // 简单处理：将密钥转换为整数并返回共享密钥
     int key_as_int = 0;
     for (auto byte : secret_bytes) {
         key_as_int = (key_as_int << 8) | byte;
