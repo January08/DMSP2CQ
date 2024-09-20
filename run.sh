b52bb641c1b6a674b6a4f55c903035f6cd034af1
January08 <150583044+January08@users.noreply.github.com>
2024-09-20 21:07:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ elif [ $# -eq 1 ];then
 	count=$1
 fi
 
-# 初始化统计变量
+
 total_time_vrf=0.0
 total_time_oprf1_client=0.0
 total_time_oprf2_client=0.0
@@ -35,12 +35,12 @@ do
     echo "begin client"
     ./build/bin/gcf_psi -r 1 -a 127.0.0.1 -p 31000 -c 1 -s 4096 -n 20 -y $protocol >> client.log 
     wait
-    # 读取服务器和客户端日志以收集数据
+
 
     last_client_log=$(tail -n 6 client.log)
     last_server_log=$(tail -n 6 server.log)
     
-    # 服务器日志
+
 
     total_time_oprf1_server=$(echo $total_time_oprf1_server + $(echo $last_server_log | grep -oP 'Time for OPRF1 \K[0-9\.]+') | bc)
     total_time_oprf2_server=$(echo $total_time_oprf2_server + $(echo $last_server_log | grep -oP 'Time for OPRF2 \K[0-9\.]+') | bc)
@@ -48,18 +48,17 @@ do
     total_time_psm_server=$(echo $total_time_psm_server + $(echo $last_server_log | grep -oP 'Timing for PSM \K[0-9\.]+') | bc)
     total_time_runtime_server=$(echo $total_time_runtime_server + $(echo $last_server_log | grep -oP 'Total runtime w/o base OTs:\K[0-9\.]+') | bc)
 
-    # 客户端日志
     total_time_oprf1_client=$(echo $total_time_oprf1_client + $(echo $last_client_log | grep -oP 'Time for OPRF1 \K[0-9\.]+') | bc)
     total_time_oprf2_client=$(echo $total_time_oprf2_client + $(echo $last_client_log | grep -oP 'Time for OPRF2 \K[0-9\.]+') | bc)
     total_time_hint_comp_client=$(echo $total_time_hint_comp_client + $(echo $last_client_log | grep -oP 'Time for hint computation \K[0-9\.]+') | bc)
     total_time_psm_client=$(echo $total_time_psm_client + $(echo $last_client_log | grep -oP 'Timing for PSM \K[0-9\.]+') | bc)
     total_time_runtime_client=$(echo $total_time_runtime_client + $(echo $last_client_log | grep -oP 'Total runtime w/o base OTs:\K[0-9\.]+') | bc)
-    #每次间隔2s
+
     sleep 2
 
 done
 
-# 计算平均值
+
 
 average_oprf1_server=$(echo "$total_time_oprf1_server / $count" | bc -l)
 average_oprf2_server=$(echo "$total_time_oprf2_server / $count" | bc -l)
@@ -72,7 +71,7 @@ average_hint_comp_client=$(echo "$total_time_hint_comp_client / $count" | bc -l)
 average_psm_client=$(echo "$total_time_psm_client / $count" | bc -l)
 average_runtime_client=$(echo "$total_time_runtime_client / $count" | bc -l)
 
-# 输出平均结果
+
 echo "Average Server Times:"
 echo "OPRF1: $average_oprf1_server ms"
 echo "OPRF2: $average_oprf2_server ms"
