103c37b93b22019600d2683ef19e5ae26664023c
January08 <150583044+January08@users.noreply.github.com>
2024-10-01 11:29:29 +0800

    Update functionalities.h

--- src/common/functionalities.h
+++ src/common/functionalities.h
@@ -13,8 +13,8 @@
 #define S_CONST 18286333650295995643
 namespace ENCRYPTO {
 
-void run_circuit_psi(const std::vector<std::uint64_t> &inputs, PsiAnalyticsContext &context, std::unique_ptr<CSocket> &sock, osuCrypto::Channel &chl);
-void run_circuit_psi3(const std::vector<std::uint64_t> &inputs, PsiAnalyticsContext &context, std::unique_ptr<CSocket> &sock,sci::NetIO* ioArr[2], osuCrypto::Channel &chl);
+void run_circuit_dmsp2cq(const std::vector<std::uint64_t> &inputs, PsiAnalyticsContext &context, std::unique_ptr<CSocket> &sock, osuCrypto::Channel &chl);
+void run_circuit_dmsp2cq3(const std::vector<std::uint64_t> &inputs, PsiAnalyticsContext &context, std::unique_ptr<CSocket> &sock,sci::NetIO* ioArr[2], osuCrypto::Channel &chl);
 
 std::unique_ptr<CSocket> EstablishConnection(const std::string &address, uint16_t port,
                                              e_role role);
